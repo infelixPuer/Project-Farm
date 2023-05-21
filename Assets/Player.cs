14754e9f1fff5adf4e46d9b454929595f6635249
Aliaksandr Kulaha <comp.science.learning.2509@gmail.com>
2023-05-21 19:44:49 +0200

    Make selection of cells and change their material

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,8 +3,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Range(0f, 10f)]
+    [SerializeField] 
+    private float _interactionDistance = 3f;
+
+    [SerializeField] 
+    private Material _selectedMaterial;
+
     private Camera _cam;
     private float _multiplier = 200f;
+    private GameObject _selectedObject;
 
     private void Awake()
     {
@@ -14,16 +22,16 @@ public class Player : MonoBehaviour
     private void Update()
     {
         if (Input.GetKey("w"))
-            transform.position += transform.forward * Time.deltaTime * 10f;
+            transform.position += 10f * Time.deltaTime * transform.forward;
 
         if (Input.GetKey("s"))
-            transform.position += -transform.forward * Time.deltaTime * 10f;
+            transform.position += 10f * Time.deltaTime * -transform.forward;
 
         if (Input.GetKey("a"))
-            transform.position += -transform.right * Time.deltaTime * 10f;
+            transform.position += 10f * Time.deltaTime * -transform.right;
 
         if (Input.GetKey("d"))
-            transform.position += transform.right * Time.deltaTime * 10f;
+            transform.position += 10f * Time.deltaTime * transform.right;
 
         var mouseX = Input.GetAxis("Mouse X");
         var mouseY = Input.GetAxis("Mouse Y");
@@ -31,12 +39,16 @@ public class Player : MonoBehaviour
         transform.Rotate(Vector3.up, mouseX * _multiplier * Time.deltaTime, Space.World);
         _cam.transform.Rotate(new Vector3(-1f, 0, 0), mouseY * _multiplier * Time.deltaTime, Space.Self);
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            var ray = new Ray(_cam.transform.position, _cam.transform.forward);
-            Physics.Raycast(ray, out RaycastHit hitInfo, 2f);
-            Debug.DrawRay(_cam.transform.position, _cam.transform.forward, Color.green);
-            Debug.Log(hitInfo.collider?.name ?? "No hits");
-        }
+        if (!Input.GetMouseButton(0)) return;
+
+        var ray = new Ray(_cam.transform.position, _cam.transform.forward);
+        Physics.Raycast(ray, out var hitInfo, _interactionDistance);
+        Debug.DrawRay(_cam.transform.position, _cam.transform.forward, Color.green);
+
+        if (hitInfo.collider == null) return;
+
+        _selectedObject = hitInfo.collider.gameObject;
+        Debug.Log($"Hit: {_selectedObject.name}");
+        _selectedObject.GetComponent<MeshRenderer>().material = _selectedMaterial;
     }
 }
