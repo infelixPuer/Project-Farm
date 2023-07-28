e113552bac1ac906ea03c8d9a0a8eb3bb338d454
Aliaksandr Kulaha <comp.science.learning.2509@gmail.com>
2023-07-28 16:11:17 +0200

    Make proper UI screen for selecting seeds

--- Assets/_Scripts/UI/CropsLoaderUI.cs
+++ Assets/_Scripts/UI/CropsLoaderUI.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using _Scripts.Player.Inventory;
+using _Scripts.UI;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,30 +13,53 @@ public class CropsLoaderUI : MonoBehaviour
     private GameObject _panel;
 
     [SerializeField] 
-    private GameObject _panelItemPrefab;
+    private ItemUI _panelItemPrefab;
 
-    private List<ItemSO> _seeds;
+    private List<Item> _seeds = new();
 
     private void Awake()
     {
-        _seeds = Resources.LoadAll<ItemSO>("Scriptables/InventoryItems/Seeds/").ToList();
+        // _seeds = Resources.LoadAll<ItemSO>("Scriptables/InventoryItems/Seeds/").ToList();
+        //
+        // foreach (var seed in _seeds)
+        // {
+        //     var item = Instantiate(_panelItemPrefab, _panel.transform);
+        //     var button = item.GetComponentInChildren<Button>();
+        //     var tmp = button.gameObject.GetComponentInChildren<TMP_Text>();
+        //
+        //     if (tmp == null)
+        //     {
+        //         Debug.LogError("Tmp is null");
+        //     }
+        //     else
+        //     {
+        //         tmp.text = seed.Name;
+        //     }
+        //     
+        //     button.onClick.AddListener(() => OnSeedSelected(seed));
+        // }
+    }
 
+    private void OnEnable()
+    {
+        _seeds = PlayerInventory.Instance.Inventory.GetItems()
+            .Select(x => x)
+            .Where(x => !x.IsEmpty && x.ItemData.ItemType.Category == ItemCategory.Seed)
+            .ToList();
+        
         foreach (var seed in _seeds)
         {
             var item = Instantiate(_panelItemPrefab, _panel.transform);
-            var button = item.GetComponentInChildren<Button>();
-            var tmp = button.gameObject.GetComponentInChildren<TMP_Text>();
-
-            if (tmp == null)
-            {
-                Debug.LogError("Tmp is null");
-            }
-            else
-            {
-                tmp.text = seed.Name;
-            }
-            
-            button.onClick.AddListener(() => OnSeedSelected(seed));
+            item.Init(seed.ItemData.Sprite, seed.Count, seed.ItemData);
+            item.SetButtonAction(() => OnSeedSelected(seed.ItemData));
+        }
+    }
+    
+    private void OnDisable()
+    {
+        foreach (Transform child in _panel.transform)
+        {
+            Destroy(child.gameObject);
         }
     }
 
