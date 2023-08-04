07b8ad76cae321145c3fae0050e7fd5b4615676f
Aliaksandr Kulaha <comp.science.learning.2509@gmail.com>
2023-08-04 12:03:39 +0200

    Make construnction materials

---
+++ Assets/_Scripts/ConstructionMaterials/Stone.cs
@@ -0,0 +1,21 @@
+﻿using _Scripts.Player.Interaction;
+using _Scripts.Player.Inventory;
+using UnityEngine;
+
+namespace _Scripts.ConstructionMaterials
+{
+    public class Stone : MonoBehaviour, IInteractable
+    {
+        public ItemSO Item;
+        public int Amount;
+
+        public void Interact(Interactor interactor)
+        {
+            if (interactor.TryGetComponent(out PlayerInventory playerInventory))
+            {
+                playerInventory.AddItem(Item, Amount);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
