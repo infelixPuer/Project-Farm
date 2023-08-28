3d59ab38de4eed8c7b949953c2eb0ac4ef0810db
Aliaksandr Kulaha <comp.science.learning.2509@gmail.com>
2023-08-28 11:11:57 +0200

    Make service for saving and loading data

---
+++ Assets/_Scripts/Services/IDataService.cs
@@ -0,0 +1,8 @@
+﻿namespace _Scripts.Services
+{
+    public interface IDataService
+    {
+        public bool SaveData<T>(T data, string relativePath);
+        public T LoadData<T>(string relativePath);
+    }
+}
---
+++ Assets/_Scripts/Services/JsonDataService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace _Scripts.Services
+{
+    public class JsonDataService : IDataService
+    {
+        public bool SaveData<T>(T data, string relativePath)
+        {
+            var path = Application.persistentDataPath + relativePath;
+            
+            try
+            {
+                if (File.Exists(path))
+                    File.Delete(path);
+
+                using FileStream stream = File.Create(path);
+                stream.Close();
+                File.WriteAllText(path, JsonConvert.SerializeObject(data));
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Unable to save file due to: {e.Message} {e.StackTrace}");
+                return false;
+            }
+        }
+
+        public T LoadData<T>(string relativePath)
+        {
+            var path = Application.persistentDataPath + relativePath;
+
+            if (!File.Exists(path))
+            {
+                Debug.LogError($"Unable to load file because {path} does not exist!");
+                throw new FileNotFoundException($"File at {path} does not exist!");
+            }
+
+            try
+            {
+                T data = JsonConvert.DeserializeObject<T>(path);
+                return data;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Unable to load data due to: {e.Message}, {e.StackTrace}");
+                throw;
+            }
+        }
+    }
+}
